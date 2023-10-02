f0af136cb230ce0a36faf89b491d6434a7ace24c
shivamjha1804 <jhashivam1804@gmail.com>
2023-10-02 21:32:44 +0530

    commit

--- 5. React Portfolio Website/portfolio/src/Components/HeroSection/index.jsx
+++ 5. React Portfolio Website/portfolio/src/Components/HeroSection/index.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import styled from "styled-components";
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div id='about'>
+      
+    </div>
   )
 }
 
